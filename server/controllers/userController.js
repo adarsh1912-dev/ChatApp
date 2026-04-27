5bb893ae504aa7afec9cda3c419dbe41142b581e
adarsh1912-dev <aj19122001@gmail.com>
2026-04-27 20:11:24 +0530

    done with userRoutes

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,7 @@
 import { generateToken } from '../lib/utils.js'
 import User from '../models/User.js'
 import bcrypt from 'bcryptjs'
+import cloudinary from '../lib/cloudinary.js'
 
 // signup a new user
 
@@ -67,4 +68,30 @@ export const login = async (req, res) => {
 
 export const checkAuth = (req, res) => {
   res.json({ success: true, user: req.user })
+}
+
+// controller to update user profile details
+
+export const updateProfile = async (req, res) => {
+
+  try {
+    const {profilePic, bio, fullName} = req.body;
+
+    const userId = req.user._id;
+    let updatedUser;
+
+    if(!profilePic){
+      updatedUser = await User.findByIdAndUpdate(userId, {bio, fullName}, {new: true})// it will return updated user object
+    }
+    else{
+      const upload = await cloudinary.uploader.upload(profilePic)
+
+      updatedUser = await User.findByIdAndUpdate(userId, {profilePic: upload.secure_url, bio, fullname}, {new: true})
+    }
+    res.json({success: true, user: updatedUser})
+  }
+  catch(error){
+    console.log(error.message)
+    res.json({success: false, message: error.message})
+  }
 }
