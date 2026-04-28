c8cb3a266d468bf73bd29f77e343ba9c41147da8
adarsh1912-dev <aj19122001@gmail.com>
2026-04-28 19:56:43 +0530

    Done with Message Model and controllers

---
+++ server/models/Message.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose'
+
+// model for user
+const messageSchema = new mongoose.Schema(
+  {
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User', // it will reference the User model so we would be able to fetch entire user document from db not just objectId
+      required: true,
+    },
+    receiverId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required },
+    text: { type: String },
+    image: { type: String },
+    seen: { type: Boolean, default: false },
+  },
+  { timestamps: true },
+)
+
+const Message = mongoose.model('Message', messageSchema)
+
+export default Message
